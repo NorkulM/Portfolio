9aa322475d5687538e63ea6755b3350d6b982379
NorkulM <martens.nicollas2@gmail.com>
2022-12-23 13:18:31 -0300

    exporting site

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
   {
     title: 'GPT-3 AI',
     description: "Using React, Node.js, Built this landing page from a Figma Design",
-      image: 'Portfolio/images/1.png',
+      image: '/images/1.png',
       tags: ['React', 'Node'],
     source: 'https://github.com/NorkulM/gpt3-nicollasm',
     visit: 'https://norkulm.github.io/gpt3-nicollasm/',
@@ -11,7 +11,7 @@ export const projects = [
   {
     title: 'Gericht Restaurant',
     description:"Another beautiful Figma Design exported to React & JavaScript. A great website to use in your Restaurant",
-    image: 'Portfolio/images/2.png',
+    image: '/images/2.png',
     tags: ['React', 'JavaScript'],
     source: 'https://github.com/NorkulM/gericht-figma-restaurant',
     visit: 'https://norkulm.github.io/gericht-figma-restaurant/',
@@ -20,7 +20,7 @@ export const projects = [
   {
     title: 'Modern Bank App',
     description: "A modern bank webpage from Figma, built with React and TailwindCSS.",
-      image: 'Portfolio/images/3.png',
+      image: '/images/3.png',
       tags: ['React', 'TailwindCSS'],
     source: 'https://github.com/NorkulM/modern-bank-landingPage',
     visit: 'https://norkulm.github.io/modern-bank-landingPage/',
@@ -29,7 +29,7 @@ export const projects = [
   {
     title: 'Under Construction',
     description: "TBA Soon",
-      image: 'Portfolio/images/4.png',
+      image: '/images/4.png',
       tags: ['React', 'TailwindCSS', 'Next.js'],
     source: '',
     visit: '',
@@ -48,5 +48,5 @@ export const TimeLineData = [
 
 export const DummyText =[{
   text: `My name is Nicollas Martens. I'm a 23 years old, and Computer Science student. My passion for technology and computer started when I was little. I started learning programming in 2014. My uncle introduced me to the Java language, it was complicated at the beginning because I had no background at all. Still,I have managed to overcome the initial struggles. When I was in college, started learning JavaScript. I loved it so much that I knew I wanted to work with it.`,
-  img: 'Portfolio/images/profile.png'
+  img: '/images/profile.png'
 }]
